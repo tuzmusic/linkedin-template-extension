02d216d5147e6ae4019490b9b5f424d21c5607bd
Jonathan Tuzman <tuzmusic@gmail.com>
2026-04-06 17:12:31 -0400

    Add packaging flow

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from 'vite';
 import { crx } from '@crxjs/vite-plugin';
 import manifest from './manifest.config';
 import preact from '@preact/preset-vite';
+import zip from 'vite-plugin-zip-pack'
 
 export default defineConfig({
-  plugins: [preact(), crx({ manifest })],
+  plugins: [
+    preact(),
+    crx({ manifest }),
+    zip({ outDir: 'release', outFileName: 'release.zip' }),
+  ],
   server: {
     port: 5173,
     strictPort: false,
