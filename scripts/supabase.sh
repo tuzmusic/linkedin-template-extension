7c1879650c4687499a741909ebfd34a824ca0999
Jonathan Tuzman <tuzmusic@gmail.com>
2026-04-24 21:20:27 -0400

    add script to serve supabase

---
+++ scripts/supabase.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# TODO: this if statement is returning truthy even when docker is already runningÅ
+
+#if ! docker info > /dev/null 2>&1; then
+#  echo -e "\nDocker daemon must be running to start Supabase. Run:\n$ open -a Docker\n\nand try again"
+#  exit 1
+#fi
+
+trap 'supabase stop' EXIT
+supabase start && supabase functions serve
